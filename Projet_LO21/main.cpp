c559f51085b7ac0d580f4264caea534e201fccb5
theguega <130441797+theguega@users.noreply.github.com>
2023-09-11 14:09:29 +0200

    test update main

--- Projet_LO21/main.cpp
+++ Projet_LO21/main.cpp
@@ -10,5 +10,7 @@
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";
+    std::cout << "Hello, World!\n";
     return 0;
 }
