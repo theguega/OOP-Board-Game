302416d2d9971d9f59e27cb14ff5618f46808448
guegathe <theo.guegan@etu.utc.fr>
2023-09-11 11:11:58 +0200

    Initialisation

---
+++ Projet_LO21/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Projet_LO21
+//
+//  Created by Theo GUEGAN on 11/09/2023.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
