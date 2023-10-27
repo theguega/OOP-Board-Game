cd8dc967993cbce97730b7adf17b644331d6ab01
Samuel.B <samuelbeziat64@gmail.com>
2023-10-27 21:04:11 +0200

    définition classe JeuCarte + qq changements

--- source/carte.cpp
+++ source/carte.cpp
@@ -14,4 +14,45 @@ ostream& operator<<(ostream& f, const Prix& p) const{
     return f;
 }
 
+JeuCarte::JeuCarte(){
+    // TODO
+}
 
+JeuCarte::~JeuCarte(){
+    for (size_t i = 0; i < getNbCartes_nv1(); ++i){
+			delete cartes_nv1[i];
+		}
+    for (size_t i = 0; i < getNbCartes_nv2(); ++i) {
+        delete cartes_nv2[i];
+    }
+    for (size_t i = 0; i < getNbCartes_nv3(); ++i) {
+        delete cartes_nv3[i];
+    }
+    for (size_t i = 0; i < getNbCartes_nobles(); ++i) {
+        delete cartes_nobles[i];
+    }
+}
+
+const Carte& JeuCarte::getCarteNiv1(size_t i) const{
+    if (i >= n1)
+        throw CarteException("Il n'y a que n1 cartes");
+    return *cartes_nv1[i];
+}
+
+const Carte& JeuCarte::getCarteNiv2(size_t i) const{
+    if (i >= n2)
+        throw CarteException("Il n'y a que n2 cartes");
+    return *cartes_nv2[i];
+}
+
+const Carte& JeuCarte::getCarteNiv3(size_t i) const{
+    if (i >= n3)
+        throw CarteException("Il n'y a que n3 cartes");
+    return *cartes_nv3[i];
+}
+
+const Carte& JeuCarte::getCarteNoble(size_t i) const{
+    if (i >= 4)
+        throw CarteException("Il n'y a que 4 cartes");
+    return *cartes_nobles[i];
+}
--- source/carte.h
+++ source/carte.h
@@ -14,7 +14,7 @@ public:
     string getInfo() const { return info; }
 };
 
-enum class Couleur {blanc, bleu, vert, noir, rouge, perle, indt}; // ajout de "indeterminé" car certaines carte ont une couleur de Bonus variable
+enum class CouleurCarte {blanc, bleu, vert, noir, rouge, perle, indt}; // ajout de "indeterminé" car certaines carte ont une couleur de Bonus variable
 
 enum class TypeCarte { Niv1, Niv2, Niv3, Royale };
 
@@ -22,12 +22,7 @@ enum class Capacite { NewTurn, TakePrivilege, TakeJetonFromBonus, TakeJetonToAdv
 
 class Prix { // on l'encapsule dans la classe carte ? ou bien il y'a d'autres éléments qui ont un cout ?
 private:
-    unsigned int blanc;
-    unsigned int bleu;
-    unsigned int vert;
-    unsigned int noir;
-    unsigned int rouge;
-    unsigned int perle;
+    unsigned int blanc, bleu, vert, noir, rouge, perle;
 public:
     Prix(unsigned int bla, unsigned int ble, unsigned int v, unsigned int n, unsigned int r, unsigned int p) : blanc(bla), bleu(ble), vert(v), noir(n), rouge(r), perle(p) {}
     unsigned int getBlanc() const { return blanc; }
@@ -42,12 +37,12 @@ ostream& operator<<(ostream& f, const Prix& p) const;
 
 class Bonus {
 private:
-    Couleur couleur;
+    CouleurCarte couleur;
     unsigned int nbBonus;
 public:
-    Bonus(Couleur c = 6, int n = 0) : couleur(c), nbBonus(n) {}
+    Bonus(CouleurCarte c = 6, int n = 0) : couleur(c), nbBonus(n) {}
     setCouleur(Couleur c) : couleur(c) {};
-    Couleur getCouleur() const { return couleur; }
+    CouleurCarte getCouleur() const { return couleur; }
     unsigned int getNbBonus() const { return nbBonus; }
 };
 
@@ -76,6 +71,7 @@ public:
 class JeuCarte {
 private:
     // TypeCarte type;
+
     array<const Carte*, n1> cartes_nv1;
     array<const Carte*, n2> cartes_nv2;
     array<const Carte*, n3> cartes_nv3;
@@ -83,6 +79,16 @@ private:
 public:
     JeuCarte();
     ~JeuCarte();
+    size_t getNbCartes_nv1() const { return n1; }
+    size_t getNbCartes_nv2() const { return n2; }
+    size_t getNbCartes_nv3() const { return n3; }
+    size_t getNbCartes_nobles() const { return 4; }
+    const Carte& getCarteNiv1(size_t i) const;
+    const Carte& getCarteNiv2(size_t i) const;
+    const Carte& getCarteNiv3(size_t i) const;
+    const Carte& getCarteNoble(size_t i) const;
+    JeuCarte(const JeuCarte& j) = delete;
+    JeuCarte& operator=(const JeuCarte& j) = delete
     // à poursuivre
 };
 
