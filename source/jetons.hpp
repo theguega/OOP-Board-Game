aed0c4a80e9d8c9d49d8f3b79bd4f21b3be0f0e3
guegathe <theo.guegan@etu.utc.fr>
2023-10-30 20:06:09 +0100

    update uml

--- source/jetons.hpp
+++ source/jetons.hpp
@@ -5,15 +5,7 @@
 #include <vector>
 #include <string>
 
-//Gestion des jetons splendor duel
-
-/* Il faudra implémenter la classe jetons, ansi que Lot de Jetons qui contiendra tous les jetons.
-Il faudra également définir le plateau de Jeu qui est une matrice de jetons.
-Les privilèges apparaitront également sur le plateau. Il faudra aussi définir la classe Lot de Privilege.
-Les jetons peuvent se situer à trois endroits : dans le sac, sur le plateau ou dans la main d'un joueur.
-Les privilèges, à deux endroits : sur le plateau ou dans la main d'un joueur.
-La classe joueur implémenter dans le fichier joueur agira comme controleur pour les jetons et les privilèges.
-*/
+//Gestion des jetons et des privileges pour splendor duel
 
 //Gestion des exceptions liées aux jetons
 class JetonException
