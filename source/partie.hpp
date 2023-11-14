b7d8b2caa4678158ae15685f8c6aa9ff35c8ae4d
Robert Antaluca <robert@antaluca.com>
2023-11-14 11:28:56 +0100

    partie.hpp

--- source/partie.hpp
+++ source/partie.hpp
@@ -16,7 +16,6 @@
 #include "strategy.hpp"
 
 //La classe partie qui initialise les joueurs, le tour et l'espace de jeux 
-class Partie {  //La classe partie qui initialise les joueurs, le tour et l'espace de jeux 
 class Partie {  
 
     //static Partie* instance;
@@ -65,51 +64,6 @@ class Partie {
         }; 
 
 
-    //static Partie* instance;
-    
-    EspaceJeux* espaceJeux = new EspaceJeux(); ;
-    Joueur* joueurs[2];
-    
-    int tour;
-    int joueurCourant;
-    
-    /*Partie( vector<Joueur*> joueurs );  // utile ?? constructeur a partir d'un vecteur de joueurs */
-
-   
-    /*
-    Partie(const Partie&) = delete;
-    Partie& operator=(const Partie&) = delete;
-    */
-    public: 
-        
-        Partie();
-
-        Partie( std::string nomJoueur1, std::string prenomJoueur1);
-            
-        Partie( std::string nomJoueur1, std::string prenomJoueur1, std::string nomJoueur2, std::string prenomJoueur2);
-
-        ~Partie() {delete joueurs[0]; delete joueurs[1]; delete espaceJeux;};
-        
-        int getTour() const { return tour; }
-        Joueur* getjoueurCourant() const { return joueurs[joueurCourant]; }
-        
-        bool finPartie();
-        void changerJoueurCourant();
-        void tour_suivant(){tour++;}
-
-        static Partie& getInstance();
-        static Partie& getInstance(std::string nomJoueur1, std::string prenomJoueur1);
-        static Partie& getInstance(std::string nomJoueur1, std::string prenomJoueur1, std::string nomJoueur2, std::string prenomJoueur2);
-
-        static void libererInstance();
-
-        void sauvegardePartie();
-        //void chargerPartie(); //si l'on décide qu'il est possible de joueur plusieurs partie en même temps
-
-        void enregisterScore();
-
-        }; 
-        
 
 //  ######  Debut du pattern Builder, pour l'instant il n'est pas utilisé comme constructeur de Partie  ######
 // Masquez cette partie si vous compilez
