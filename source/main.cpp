2042a267ad6629b539435345711319ae0e9b4d0a
Samuel.B <samuelbeziat64@gmail.com>
2023-11-21 23:15:03 +0100

    comment main

--- source/main.cpp
+++ source/main.cpp
@@ -17,23 +17,23 @@ std::filesystem::path projectPath = string("\\Users\\Beziat\\source\\repos\\Proj
 int main(int argc, const char * argv[]) {
     int i = 0;
     Controller control;
-    control.getJoueurCourant().afficherJoueur();
-    //control.lancerPartie();
+    //control.getJoueurCourant().afficherJoueur();
+    control.lancerPartie();
 
-    //while (control.getPartie().getJoueur1()->estGagnant() == false && control.getPartie().getJoueur2()->estGagnant() == false) {
-    //    // TODO
-    //    // simulation victoire d'un joueur
-    //    if (i == 5)
-    //        control.getJoueurCourant().updatePtsPrestige(16);
-    //    control.getJoueurCourant().afficherJoueur();
-    //    i++;
-    //    control.tour_suivant();
-    //}
+    while (control.getPartie().getJoueur1()->estGagnant() == false && control.getPartie().getJoueur2()->estGagnant() == false) {
+        // TODO
+        // simulation victoire d'un joueur
+        if (i == 5)
+            control.getJoueurCourant().updatePtsPrestige(16);
+        control.getJoueurCourant().afficherJoueur();
+        i++;
+        control.tour_suivant();
+    }
 
-    //if (control.getPartie().getJoueur1()->estGagnant())
-    //    cout << control.getPartie().getJoueur1()->getPseudo() << " est gagnant;"<<std::endl;
-    //else if (control.getPartie().getJoueur2()->estGagnant())
-    //    cout << control.getPartie().getJoueur2()->getPseudo() << " est gagnant;"<<std::endl;
+    if (control.getPartie().getJoueur1()->estGagnant())
+        cout << control.getPartie().getJoueur1()->getPseudo() << " est gagnant;"<<std::endl;
+    else if (control.getPartie().getJoueur2()->estGagnant())
+        cout << control.getPartie().getJoueur2()->getPseudo() << " est gagnant;"<<std::endl;
 
     //control.getPartie().getJoueur1()->afficherJoueur();
     //control.getPartie().getJoueur2()->afficherJoueur();
