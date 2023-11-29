c62751e30386d5d98af8ea7dd7dd86c37e1dcd83
Samuel.B <samuelbeziat64@gmail.com>
2023-11-29 13:15:18 +0100

    petite modif main

--- source/main.cpp
+++ source/main.cpp
@@ -12,9 +12,9 @@
 
 //Mettez ici le chemin absolue vers le projet
 //Theo
-std::filesystem::path projectPath = string("/Users/theoguegan/Documents/UTC/GI01/LO21/Projet_LO21");
+//std::filesystem::path projectPath = string("/Users/theoguegan/Documents/UTC/GI01/LO21/Projet_LO21");
 //Samuel B
-//std::filesystem::path projectPath = string("\\Users\\Beziat\\source\\repos\\Projet_LO21");
+std::filesystem::path projectPath = string("\\Users\\Beziat\\Documents\\Projet_LO21\\source");
 //Robert
 //std::filesystem::path projectPath = string("/Users/robertantaluca/Desktop/Projet_LO21");
 //Samuel M
@@ -48,8 +48,8 @@ std::filesystem::path projectPath = string("/Users/theoguegan/Documents/UTC/GI01
 
 int main(void) {
     Controller control;
-    control.getJoueurCourant().afficherJoueur();
     control.lancerPartie();
+    control.getJoueurCourant().afficherJoueur();
 
     // simulation victoire d'un joueur
     int i = 0;
