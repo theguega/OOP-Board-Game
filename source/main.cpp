45ff999a31c3d666fd837a2562f274abd800089e
guegathe <theo.guegan@etu.utc.fr>
2023-11-29 12:45:41 +0100

    separation interface / terminal

--- source/main.cpp
+++ source/main.cpp
@@ -12,9 +12,9 @@
 
 //Mettez ici le chemin absolue vers le projet
 //Theo
-//std::filesystem::path projectPath = string("/Users/theoguegan/Documents/UTC/GI01/LO21/Projet_LO21");
+std::filesystem::path projectPath = string("/Users/theoguegan/Documents/UTC/GI01/LO21/Projet_LO21");
 //Samuel B
-std::filesystem::path projectPath = string("\\Users\\Beziat\\source\\repos\\Projet_LO21");
+//std::filesystem::path projectPath = string("\\Users\\Beziat\\source\\repos\\Projet_LO21");
 //Robert
 //std::filesystem::path projectPath = string("/Users/robertantaluca/Desktop/Projet_LO21");
 //Samuel M
@@ -22,9 +22,70 @@ std::filesystem::path projectPath = string("\\Users\\Beziat\\source\\repos\\Proj
 //Maxime
 
 
-int main(int argc, char * argv[]) {
-    QApplication a(argc, argv);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//####################################
+//###### Partie en mode Terminal #####
+//####################################
+
+
+int main(void) {
+    Controller control;
+    control.getJoueurCourant().afficherJoueur();
+    control.lancerPartie();
+
+    // simulation victoire d'un joueur
     int i = 0;
+    if (i == 5)
+        control.getJoueurCourant().updatePtsPrestige(16);
+    control.getJoueurCourant().afficherJoueur();
+    i++;
+    control.tour_suivant();
+
+    if (control.getPartie().getJoueur1()->estGagnant())
+        cout << control.getPartie().getJoueur1()->getPseudo() << " est gagnant;"<<std::endl;
+    else if (control.getPartie().getJoueur2()->estGagnant())
+        cout << control.getPartie().getJoueur2()->getPseudo() << " est gagnant;"<<std::endl;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//avec boucle de jeux
+
+/*
+
+int main(void) {
     Controller control;
     control.getJoueurCourant().afficherJoueur();
     control.lancerPartie();
@@ -35,173 +96,175 @@ int main(int argc, char * argv[]) {
         // tour pour chacun des joueurs
         for (unsigned int i = 0; i < 2; i++) {
             control.getJoueurCourant().afficherJoueur();
-            
+
             unsigned int etat_tour = 0;
             while (etat_tour != 4) {
                 // actions optionelles
                 switch (etat_tour) {
-                    case 0:
-                        unsigned int etat_action = 0;
-                        while (etat_action != 4) {
-                            switch (etat_action)
-                                {
-                                case 0:
-                                    std::cout << "Actions optionnelles: " << endl;
-                                    std::cout << "1. Utiliser un privilège" << endl;
+                case 0:
+                    unsigned int etat_action = 0;
+                    while (etat_action != 4) {
+                        switch (etat_action)
+                        {
+                        case 0:
+                            std::cout << "Actions optionnelles: " << endl;
+                            std::cout << "1. Utiliser un privilège" << endl;
                                     std:: cout << "2. Remplir le plateau" << endl;
-                                    std::cout << "3. Ne plus faire d'actions optionnelles" << endl;
-                                    std::cout << "Votre choix (1/2/3): ";
-                                    cin >> etat_action;
-                                    std::cout << endl;
-                                    break;
-
-                                case 1:
-                                    try
-                                    {
-                                        control.utiliserPrivilege(control.getPartie().getEspaceJeux().getPlateau());
-                                        etat_action = 0;
-                                    }
-                                    catch(const std::exception& e)
-                                        {
-                                            std::cout << e.what() << '\n';
-                                            etat_action = 0;
-                                    }
-                                    break;
-                                case 2:
-                                    try
-                                    {
-                                        control.remplirPlateau(control.getPartie().getEspaceJeux().getPlateau(), control.getPartie().getEspaceJeux().getSac(), control.getJoueurAdverse());
-                                        etat_action = 0;
-                                    }
-                                    catch(const std::exception& e)
-                                        {
-                                            std::cout << e.what() << '\n';
-                                            etat_action = 0;
-                                    }
-                                    break;
-
-                                case 3:
-                                    
-                                    etat_tour = 1;
-                                    etat_action = 4;
-                                    break;
-
-                                default:
-                                        break;
-                                    }
-
-                        }
-                        break;
-
+                            std::cout << "3. Ne plus faire d'actions optionnelles" << endl;
+                            std::cout << "Votre choix (1/2/3): ";
+                            cin >> etat_action;
+                            std::cout << endl;
+                            break;
 
                         case 1:
-                        unsigned int etat_action = 0;
-                        while (etat_action != 4) {
-                            switch (etat_action)
-                                {
-                                case 0:
-                                    std::cout << "Actions obligatoires: " << endl;
-                                    std::cout << "1. Récupérer des jetons" << endl;
-                                    std:: cout << "2. Acheter une carte joaillerie" << endl;
-                                    std::cout << "3. Réserver une carte" << endl;
-                                    cin >> etat_action;
-                                    std::cout << endl;
-                                    break;
-                                case 1:
-                                    try
-                                    {
-                                        control.recupererJetons(control.getPartie().getEspaceJeux().getPlateau());
-                                        etat_action = 4;
-                                    }
-                                    catch(const std::exception& e)
-                                        {
-                                            std::cout << e.what() << '\n';
-                                            etat_action = 0;
-                                    }
-                                    break;
-                                case 2:
-                                    try
-                                    {
-                                        control.acheterCarteJoaillerie(control.getPartie().getEspaceJeux().getPyramide());
-                                        etat_action = 4;
-                                    }
-                                    catch(const std::exception& e)
-                                        {
-                                            std::cout << e.what() << '\n';
-                                            etat_action = 0;
-                                    }
-                                    break;
-                                case 3:
-                                    try
-                                    {
-                                        control.orReserverCarte(control.getPartie().getEspaceJeux().getPyramide(), control.getPartie().getEspaceJeux().getPlateau());
-                                        etat_action = 4;
-                                    }
-                                    catch(const std::exception& e)
-                                        {
-                                            std::cout << e.what() << '\n';
-                                            etat_action = 0;
-                                    }
-                                    break;
-                                default:
-                                        break;
-                                    }
-                            etat_tour = 2;
+                            try
+                            {
+                                control.utiliserPrivilege(control.getPartie().getEspaceJeux().getPlateau());
+                                etat_action = 0;
+                            }
+                            catch(const std::exception& e)
+                            {
+                                std::cout << e.what() << '\n';
+                                etat_action = 0;
+                            }
+                            break;
+                        case 2:
+                            try
+                            {
+                                control.remplirPlateau(control.getPartie().getEspaceJeux().getPlateau(), control.getPartie().getEspaceJeux().getSac(), control.getJoueurAdverse());
+                                etat_action = 0;
+                            }
+                            catch(const std::exception& e)
+                            {
+                                std::cout << e.what() << '\n';
+                                etat_action = 0;
+                            }
+                            break;
 
+                        case 3:
+
+                            etat_tour = 1;
+                            etat_action = 4;
+                            break;
+
+                        default:
+                            break;
                         }
-                        break;
-                    case 2:
+
+                    }
+                    break;
+
+
+                case 1:
+                    etat_action = 0;
+                    while (etat_action != 4) {
+                        switch (etat_action)
+                        {
+                        case 0:
+                            std::cout << "Actions obligatoires: " << endl;
+                            std::cout << "1. Récupérer des jetons" << endl;
+                                    std:: cout << "2. Acheter une carte joaillerie" << endl;
+                            std::cout << "3. Réserver une carte" << endl;
+                                        cin >> etat_action;
+                            std::cout << endl;
+                            break;
+                        case 1:
+                            try
+                            {
+                                control.recupererJetons(control.getPartie().getEspaceJeux().getPlateau());
+                                etat_action = 4;
+                            }
+                            catch(const std::exception& e)
+                            {
+                                std::cout << e.what() << '\n';
+                                etat_action = 0;
+                            }
+                            break;
+                        case 2:
+                            try
+                            {
+                                control.acheterCarteJoaillerie(control.getPartie().getEspaceJeux().getPyramide());
+                                etat_action = 4;
+                            }
+                            catch(const std::exception& e)
+                            {
+                                std::cout << e.what() << '\n';
+                                etat_action = 0;
+                            }
+                            break;
+                        case 3:
+                            try
+                            {
+                                control.orReserverCarte(control.getPartie().getEspaceJeux().getPyramide(), control.getPartie().getEspaceJeux().getPlateau());
+                                etat_action = 4;
+                            }
+                            catch(const std::exception& e)
+                            {
+                                std::cout << e.what() << '\n';
+                                etat_action = 0;
+                            }
+                            break;
+                        default:
+                            break;
+                        }
+                        etat_tour = 2;
+
+                    }
+                    break;
+                case 2:
                     //verif de fin de tout
                     break;
 
-                                
-                    default:
-                        break;
+
+                default:
+                    break;
                 }
             }
-                
-        
+
+
         }
 
         control.setJoueurCourant(i); // dommage que changerJoueurCourant() ai disparu, je trouve cette solution moins élégante
     }
 
+    return 0;
+}
 
-    /*
-        // simulation victoire d'un joueur
-        if (i == 5)
-            control.getJoueurCourant().updatePtsPrestige(16);
-        control.getJoueurCourant().afficherJoueur();
-        i++;
-        control.tour_suivant();
-    */
-    if (control.getPartie().getJoueur1()->estGagnant())
-        cout << control.getPartie().getJoueur1()->getPseudo() << " est gagnant;"<<std::endl;
-    else if (control.getPartie().getJoueur2()->estGagnant())
-        cout << control.getPartie().getJoueur2()->getPseudo() << " est gagnant;"<<std::endl;
-
-//    //control.getPartie().getJoueur1()->afficherJoueur();
-//    //control.getPartie().getJoueur2()->afficherJoueur();
-//    cout << control.getPartie().getEspaceJeux().getPlateau();
-    //control.getPartie().getEspaceJeux().getPyramide().afficherPyramide();
-
-    //control.enregisterScore();
-    //control.sauvegardePartie();
+*/
 
 
-    //#########################################
-    // ##### Partie avec l'application Qt #####
-    //#########################################
 
-    //QApplication app(argc, argv);
 
-    /*vueCarte carte = vueCarte(nullptr);
-    carte.show();*/
 
-    /*pageCreation page = pageCreation(nullptr);
-    page.show();*/
 
-    /*QWidget* widget = new QWidget;
+
+
+
+
+
+
+
+
+
+
+
+
+//#########################################
+//###### Partie avec l'application Qt #####
+//#########################################
+
+/*
+
+int main(int argc, char * argv[]) {
+    QApplication a(argc, argv);
+
+    vueCarte carte = vueCarte(nullptr);
+    carte.show();
+
+    pageCreation page = pageCreation(nullptr);
+    page.show();
+
+    QWidget* widget = new QWidget;
 
     vueJeton* jeton = new vueJeton(nullptr);
 
@@ -210,16 +273,15 @@ int main(int argc, char * argv[]) {
 
     widget -> setLayout(layout);
 
-    widget->show();*/
+    widget->show();
 
-    /*vuePlateau* plateau = new vuePlateau(nullptr, 500, 500);
-    plateau -> show();*/
+    vuePlateau* plateau = new vuePlateau(nullptr, 500, 500);
+    plateau -> show();
 
-    //toutesPages* page = new toutesPages(nullptr, &app);
-    //page->show();
+    toutesPages* page = new toutesPages(nullptr, &app);
+    page->show();
 
-    //return app.exec();
-
-
-    return a.exec();
+    return app.exec();
 }
+
+*/
