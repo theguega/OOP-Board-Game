da803662638dc0151d543281b87242eb31339c32
Robert Antaluca <robert@antaluca.com>
2023-12-02 21:56:18 +0100

    9 pour quitter

--- source/main.cpp
+++ source/main.cpp
@@ -140,6 +140,12 @@ int main(void) {
                             break;
                         }
 
+                        case 9:{
+
+                            return 0;
+
+                        }
+
                         default:{
                             break;}
                         }
@@ -292,6 +298,13 @@ int main(void) {
                     std::cout << "Fin de partie";
                     etat_tour = 4;
                     break;
+                }
+
+
+                case 9:{
+
+                    return 0;
+
                 }
 
 
