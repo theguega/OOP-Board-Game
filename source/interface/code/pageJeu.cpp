2290a063207a9444f8feb501c397b457c0162529
Samuel.B <samuelbeziat64@gmail.com>
2023-12-13 20:27:36 +0100

    la pyramide ca marche

--- source/interface/code/pageJeu.cpp
+++ source/interface/code/pageJeu.cpp
@@ -7,6 +7,7 @@
 
 pageJeu::pageJeu(QString statut_partie, QString pseudo_j_1, type type_j_1, QString pseudo_j_2, type type_j_2, QWidget *parent) : QWidget(parent) {
     control = new Controller(statut_partie, pseudo_j_1, type_j_1, pseudo_j_2, type_j_2);
+    control->lancerPartie();
     ecran = QGuiApplication::primaryScreen();
     tailleEcran = ecran->availableGeometry().size();
 
@@ -45,7 +46,6 @@ pageJeu::pageJeu(QString statut_partie, QString pseudo_j_1, type type_j_1, QStri
     aSauvegarde = new popUpValider(nullptr, "quitter sans sauvegarder");
     connect(aSauvegarde -> getBoutonOui(), &QPushButton::clicked, this, &pageJeu::quitter);
     connect(aSauvegarde -> getBoutonNon(), &QPushButton::clicked, this, &pageJeu::rester);
-    control->lancerPartie();
 }
 
 void pageJeu::paintEvent(QPaintEvent *event){
